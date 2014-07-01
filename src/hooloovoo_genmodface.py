ddac2a4684b840d3536e8b0cf0a43c7bffa46373
Ubuntu <ubuntu@ip-172-31-11-125.us-west-2.compute.internal>
2014-07-01 21:40:53 +0000

    Experiments yield best result yet

--- src/hooloovoo_genmodface.py
+++ src/hooloovoo_genmodface.py
@@ -254,10 +254,13 @@ reload(hlv_models)
 reload(hlv_train)
 lr = 0.05
 lr_exp = 0.5
-L2_reg = 0.00
+L2_reg = 0.0
 batch_size = 50
 
 """
+         ( 'conv'  ,    {	'n_filters':16,     
+				'filter':(7,7)
+			}), 
          ( 'pooling',   {	'poolsize':(3,3)	
 			}),
          ( 'conv'  ,    {	'n_filters':32,     
@@ -276,25 +279,71 @@ batch_size = 50
 #         ( 'dropout',   {	'prob':0.5
 #			}), 
 specs = [
+	 (  'conv'   ,  {	'n_filters':	128,
+				'filter':	(7,7),
+			}), 
+         ( 'pooling',   {	'poolsize':	(2,2)
+			}),
+	 (  'conv'   ,  {	'n_filters':	256,
+				'filter':	(5,5),
+			}), 
+         ( 'pooling',   {	'poolsize':	(2,2)
+			}),
+	 (  'conv'   ,  {	'n_filters':	512,
+				'filter':	(4,4),
+			}), 
+         ( 'pooling',   {	'poolsize':	(2,2)
+			}),
+         ( 'hidden'  ,  {	'n_out':1000
+			}), 
+         ( 'hidden'  ,  {	'n_out':250
+			}), 
+         ( 'hidden'  ,  {	'n_out':50
+			}), 
+         ( 'logistic',  {	'n_out':2
+			})
+]
+specs = [
+	 (  'conv'   ,  {	'n_filters':	32,
+				'filter':	(9,9),
+			}), 
+         ( 'pooling',   {	'poolsize':	(2,2)
+			}),
 	 (  'conv'   ,  {	'n_filters':	64,
 				'filter':	(5,5),
 			}), 
          ( 'pooling',   {	'poolsize':	(2,2)
 			}),
 	 (  'conv'   ,  {	'n_filters':	128,
-				'filter':	(4,4),
-			}), 
-         ( 'pooling',   {	'poolsize':	(2,2)
-			}),
-	 (  'conv'   ,  {	'n_filters':	256,
 				'filter':	(3,3),
 			}), 
          ( 'pooling',   {	'poolsize':	(2,2)
 			}),
          ( 'hidden'  ,  {	'n_out':500
 			}), 
-         ( 'hidden'  ,  {	'n_out':100
+         ( 'hidden'  ,  {	'n_out':50
 			}), 
+         ( 'logistic',  {	'n_out':2
+			})
+]
+specs =  [
+         ( 'conv'  ,    {	'n_filters':16,
+				'filter':(7,7)
+			}), 
+         ( 'pooling',   {	'poolsize':(2,2)	
+			}),
+         ( 'conv'  ,    {	'n_filters':32,  
+				'filter':(5,5)
+			}), 
+         ( 'pooling',   {	'poolsize':(2,2)	
+			}),
+         ( 'conv'  ,    {	'n_filters':64,
+				'filter':(3,3)
+			}), 
+         ( 'pooling',   {	'poolsize':(2,2)
+			}),
+         ( 'hidden'  ,  {	'n_out':50
+			}),
          ( 'logistic',  {	'n_out':2
 			})
 ]
@@ -304,13 +353,15 @@ M = Generic_model(n_in = (64,64), n_out = 2, data= data_faces, layerSpecs = spec
 
 print "Model Magnitude:", np.log(np.sum([np.prod(p.eval().shape) for p in M.params]))/np.log(2)
 
+M.lr.set_value(np.float32(lr))
+param_values = Train_minibatches(M, min_epochs=200, max_epochs=1500, validation_frequency=41)
+"""
 param_values = hlv_aux.get_params(M)
 
 while lr > 0.00005:
     print "============ LR %f" % (lr)
-    M.lr.set_value(np.float32(lr))
     #hlv_aux.set_params(M, param_values)
     #param_values = hlv_train.train_minibatches(M, min_epochs=20, max_epochs=200)
     param_values = Train_minibatches(M, min_epochs=50, max_epochs=1000, validation_frequency=39)
     lr = lr * lr_exp
-
+"""
